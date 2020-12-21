656749f4da96113135d5dca0993623e6721edce7
Tim Holzer <timiholzer@gmail.com>
2020-12-21 07:58:40 -0700

    fixed error with user route

--- models/post.js
+++ models/post.js
@@ -1,63 +1,40 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {
-    static upvote(body, models) {
-        return models.Vote.create({
-          user_id: body.user_id,
-          post_id: body.post_id
-        }).then(() => {
-          return Post.findOne({
-            where: {
-              id: body.post_id
-            },
-            attributes: [
-              'id',
-              'post_url',
-              'title',
-              'created_at',
-            ]
-          });
-        });
-      }
+class Post extends Model { 
     }
 
 
     Post.init(
-        {
+      {
         id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
         },
         title: {
-            type: DataTypes.STRING,
-            allowNull: false
+          type: DataTypes.STRING,
+          allowNull: false
         },
-        post_url: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-            validate: {
-            len: [1]
-        }
-            
-        
+        content: {
+          type: DataTypes.TEXT,
+          allowNull: false
         },
         user_id: {
-            type: DataTypes.INTEGER,
-            references: {
+          type: DataTypes.INTEGER,
+          references: {
             model: 'user',
             key: 'id'
-            }
+          }
         }
-        },
-        {
+      },
+      {
         sequelize,
         freezeTableName: true,
         underscored: true,
         modelName: 'post'
-        }
+      }
     );
     
     module.exports = Post;
