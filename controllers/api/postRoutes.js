76248c7a6f6aeb535bbf2ac628e4afb82703c11d
Tim Holzer <timiholzer@gmail.com>
2021-02-22 11:58:15 -0700

    creating a post works and added a few screenshots

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
   Post.findAll({
     attributes: [
       'id', 
-      'post_url', 
+      'content', 
       'title', 
       'created_at',
     ],
@@ -49,7 +49,7 @@ router.get('/:id', (req, res) => {
     },
     attributes: [
       'id', 
-      'post_url', 
+      'content', 
       'title', 
       'created_at',
     ],
@@ -91,7 +91,7 @@ router.get('/:id', (req, res) => {
  router.post('/', withAuth, (req, res) => {
   Post.create({
     title: req.body.title,
-    post_url: req.body.post_url,
+    content: req.body.content,
     user_id: req.session.user_id
   })
     .then(postData => res.json(postData))
--- models/post.js
+++ models/post.js
@@ -19,7 +19,7 @@ class Post extends Model {
         },
         content: {
           type: DataTypes.TEXT,
-          allowNull: false
+          allowNull: true
         },
         user_id: {
           type: DataTypes.INTEGER,
